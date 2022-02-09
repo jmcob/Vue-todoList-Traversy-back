b54bd8d56f25d6217c9f6dbcdba7f1f34fa8f4b0
jmcob <jmcob@pm.me>
2022-02-09 16:10:45 +0100

    test

--- api/controllers/tasks.js
+++ api/controllers/tasks.js
@@ -4,35 +4,13 @@ const User = require("../models/User");
 const ObjectID = require("mongoose").Types.ObjectId;
 
 // création of a task by user
-// exports.createTask = (req, res, next) => {
-//         console.log(req.body);
-//         const taskObject = req.body;
-//         // delete taskObject.id;
-//         const task = new Task({
-//                 ...taskObject,
-//         });
-//         task.save()
-//                 .then(() => res.status(201).json(task))
-//                 .catch((error) => res.status(400).json({ error }));
-// };
-
 module.exports.createTask = (req, res) => {
         if (!ObjectID.isValid(req.params.id))
                 return res.status(400).send("ID unknown : " + req.params.id);
 
         try {
-                // let user = await User.findOne({ _id: req.params.id });
-                // user.tasks.push(req.body);
-                // await user
-                //         .save()
-                //         .then((data) => res.json(data))
-                //         .catch((err) =>
-                //                 res.status(400).send({
-                //                         message: err,
-                //                 })
-                //         );
-                return User.findOneAndUpdate(
-                        (_id = req.params.id),
+                return User.findByIdAndUpdate(
+                        req.params.id,
                         {
                                 $push: {
                                         tasks: {
@@ -40,6 +18,87 @@ module.exports.createTask = (req, res) => {
                                                 day: req.body.day,
                                                 reminder: req.body.reminder,
                                                 // timestamp: new Date().getTime(),
+                                        },
+                                },
+                        },
+                        { new: true },
+                        (err, docs) => {
+                                if (!err) return res.json(docs);
+                                else return res.status(400).send(err);
+                        }
+                );
+        } catch (err) {
+                return res.status(400).send(err);
+        }
+};
+
+//user id getting ONE task
+exports.getOneTask = async (req, res, next) => {
+        let user = await User.findOne({
+                _id: req.params.id,
+        });
+        let tasks = user.tasks;
+        if (tasks) return res.status(200).json(tasks);
+        else return res.status(400).send(err);
+};
+
+// User is updating the reminder of a task
+exports.modifyTask = async (req, res, next) => {
+        if (!ObjectID.isValid(req.params.id))
+                return res.status(400).send("ID unknown : " + req.params.id);
+
+        try {
+                let userTaskToUpdate = await User.findOne({
+                        _id: req.params.id,
+                });
+                userTaskToUpdate.tasks.find(
+                        (tasks) =>
+                                tasks._id.equals(req.body.taskId).reminder ==
+                                !userTaskToUpdate.tasks.find(
+                                        (tasks) =>
+                                                tasks._id.equals(
+                                                        req.body.taskId
+                                                ).reminder
+                                )
+                );
+                userTaskToUpdate
+                        .save()
+                        .then((data) => res.status(200).json(data))
+                        .catch((error) => res.status(400).json({ error }));
+                //         return User.findOneAndUpdate(
+                //                 { _id: req.body.taskId },
+                //                 (err, user) => {
+                //                         let theTask = user.tasks.find((task) =>
+                //                                 task._id.equals(req.body.taskId)
+                //                         );
+                //                         if (!theTask)
+                //                                 return res
+                //                                         .status(404)
+                //                                         .send("Task not found");
+                //                         theTask.reminder = !theTask.reminder;
+                //                         return user.save((err) => {
+                //                                 if (!err)
+                //                                         return res
+                //                                                 .status(200)
+                //                                                 .send(docs);
+                //                                 return res.status(500).send(err);
+                //                         });
+                //                 }
+                //         );
+        } catch (err) {
+                return res.status(500).send(err);
+        }
+};
+
+// User is deleting one of his sauces
+exports.deleteTask = (req, res, next) => {
+        try {
+                return User.findByIdAndUpdate(
+                        req.params.id,
+                        {
+                                $pull: {
+                                        tasks: {
+                                                _id: req.params.id,
                                         },
                                 },
                         },
@@ -54,52 +113,10 @@ module.exports.createTask = (req, res) => {
         }
 };
 
-//user id getting ONE task
-exports.getOneTask = (req, res, next) => {
-        User.findOne({
-                _id: req.params.id,
-        })
-                .then((data) => {
-                        res.status(200).json(data);
-                })
-                .catch((error) => {
-                        res.status(404).json({
-                                error: error,
-                        });
-                });
-};
-
-// User is updating a task he created
-// exports.modifyTask = async (req, res, next) => {
-//         let taskToUpdate = await Task.findOne({ _id: req.params.id });
-//         // delete taskObject._id;
-//         taskToUpdate.reminder = !taskToUpdate.reminder;
-//         taskToUpdate
-//                 .save()
-//                 .then((data) => res.status(200).json(data))
-//                 .catch((error) => res.status(400).json({ error }));
-// };
-
-// User is deleting one of his sauces
-exports.deleteTask = (req, res, next) => {
-        Task.deleteOne({ id: req.params.id })
-                .then(() =>
-                        res.status(200).json({
-                                message: "Task supprimé !",
-                        })
-                )
-                .catch((error) => res.status(400).json({ error }));
-};
-
 // User is getting ALL tasks
-exports.getAllTasks = (req, res, next) => {
-        Task.find()
-                .then((tasks) => {
-                        res.status(200).json(tasks);
-                })
-                .catch((error) => {
-                        res.status(400).json({
-                                error: error,
-                        });
-                });
+exports.getAllTasks = async (req, res, next) => {
+        let user = await User.findOne({ _id: req.params.id });
+        let tasks = user.tasks;
+        if (tasks) return res.status(200).json(tasks);
+        else return res.status(400).send(err);
 };
--- api/routes/tasks.js
+++ api/routes/tasks.js
@@ -1,11 +1,12 @@
 const express = require("express");
 const router = express.Router();
 const tasksCtrl = require("../controllers/tasks");
-// routes CRUD of tasks
-router.get("/", tasksCtrl.getAllTasks);
+
+// routes CRUD of user tasks
+router.get("/:id", tasksCtrl.getAllTasks);
 router.patch("/:id", tasksCtrl.createTask);
-router.get("/:id", tasksCtrl.getOneTask);
-// router.put("/:id", tasksCtrl.modifyTask);
+// router.get("/:id", tasksCtrl.getOneTask);
+router.put("/reminder/:id", tasksCtrl.modifyTask);
 router.delete("/:id", tasksCtrl.deleteTask);
 
 module.exports = router;
